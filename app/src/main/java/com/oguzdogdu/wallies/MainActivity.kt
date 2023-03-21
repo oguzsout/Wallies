a4c9a373ef3858354dbe49a6ffd36e3cb205f810
oguzhandogdu <oguzd494@gmail.com>
2023-03-21 13:38:21 +0300

    Initial commit

---
+++ app/src/main/java/com/oguzdogdu/wallies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oguzdogdu.wallies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
