4b75c13e5967899b231d332aabf24a5455a95436
oguzhandogdu <oguzd494@gmail.com>
2023-03-24 23:30:38 +0300

    Add splash screen feature

---
+++ app/src/main/java/com/oguzdogdu/wallies/core/WalliesApp.kt
@@ -0,0 +1,7 @@
+package com.oguzdogdu.wallies.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WalliesApp : Application()
---
+++ app/src/main/java/com/oguzdogdu/wallies/presentation/SplashFragment.kt
@@ -0,0 +1,10 @@
+package com.oguzdogdu.wallies.presentation
+
+import com.oguzdogdu.wallies.core.BaseFragment
+import com.oguzdogdu.wallies.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    override fun initViews() {
+        super.initViews()
+    }
+}
