4da211f69de0d988c9201576c4c79c155d30a4c1
oguzhandogdu <oguzd494@gmail.com>
2023-03-26 14:56:04 +0300

    Remove api key

--- app/src/main/java/com/oguzdogdu/wallies/utils/Constants.kt
+++ app/src/main/java/com/oguzdogdu/wallies/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.oguzdogdu.wallies.utils
 
 object Constants {
     const val UNSPLASH_BASE_URL = "https://api.unsplash.com/"
-    const val API_KEY = "7_lC__UQscpZgqX_A46apWYsYY2IeBaFXaJ_S-LHATk"
+    const val API_KEY = ""
     const val PAGE_ITEM_LIMIT = 30
     const val POPULAR = "popular"
     const val LATEST = "latest"
