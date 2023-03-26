ce6bff44bb3280875a17c7828d4e48f1b4328e26
oguzhandogdu <oguzd494@gmail.com>
2023-03-27 01:14:14 +0300

    Add main fragment and edit dependecies management

--- app/src/main/java/com/oguzdogdu/wallies/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/wallies/MainActivity.kt
@@ -2,10 +2,15 @@ package com.oguzdogdu.wallies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oguzdogdu.wallies.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/oguzdogdu/wallies/presentation/SplashFragment.kt
+++ app/src/main/java/com/oguzdogdu/wallies/presentation/SplashFragment.kt
@@ -1,10 +1,22 @@
 package com.oguzdogdu.wallies.presentation
 
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.oguzdogdu.wallies.core.BaseFragment
 import com.oguzdogdu.wallies.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
+@AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
     override fun initViews() {
         super.initViews()
+        lifecycleScope.launchWhenCreated {
+            goToMainFragment()
+        }
+    }
+    private suspend fun goToMainFragment() {
+        delay(3000)
+       findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToMainFragment2())
     }
 }
