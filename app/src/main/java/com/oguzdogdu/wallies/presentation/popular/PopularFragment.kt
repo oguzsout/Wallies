4d9c656ed5847134cc2d5df5f7a8a7c37a209cdb
oguzhandogdu <oguzd494@gmail.com>
2023-03-30 02:12:17 +0300

    Add shimmer effect

---
+++ app/src/main/java/com/oguzdogdu/wallies/presentation/popular/PopularFragment.kt
@@ -0,0 +1,53 @@
+package com.oguzdogdu.wallies.presentation.popular
+
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.oguzdogdu.domain.Resource
+import com.oguzdogdu.wallies.core.BaseFragment
+import com.oguzdogdu.wallies.databinding.FragmentPopularBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+@AndroidEntryPoint
+class PopularFragment : BaseFragment<FragmentPopularBinding>(FragmentPopularBinding::inflate) {
+    private val viewModel: PopularViewModel by viewModels()
+    private val mainWallpaperAdapter = MainWallpaperAdapter()
+    override fun initViews() {
+        super.initViews()
+        binding.apply {
+            recyclerViewWallpapers.adapter = mainWallpaperAdapter
+            val layoutManager = GridLayoutManager(requireContext(), 2)
+            recyclerViewWallpapers.layoutManager = layoutManager
+            recyclerViewWallpapers.setHasFixedSize(true)
+        }
+    }
+
+    override fun observeData() {
+        super.observeData()
+        lifecycleScope.launchWhenStarted {
+            viewModel.getPopular.collectLatest { result ->
+                when {
+                    result.isLoading -> {
+                        binding.shimmerLayout.startShimmer()
+                    }
+                    result.error.isNotEmpty() -> {
+                        Snackbar.make(requireView(), "Check Connectivity", Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+                    result.popular.isNotEmpty() -> {
+                        with(binding){
+                            shimmerLayout.apply {
+                                visibility = View.GONE
+                                stopShimmer()
+                                mainWallpaperAdapter.submitList(result.popular)
+                                recyclerViewWallpapers.visibility = View.VISIBLE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
