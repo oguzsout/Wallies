1827cdd819fc06ef9abe8cb5f4c8851f3e322fd9
oguzhandogdu <oguzd494@gmail.com>
2023-04-06 22:15:25 +0300

    Added logos and fonts

--- app/src/main/java/com/oguzdogdu/wallies/util/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.oguzdogdu.wallies.util
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
-}
