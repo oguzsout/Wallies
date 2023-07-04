858200a54ed59df39434dfef002de33b24a8f437
oguzhandogdu <oguzd494@gmail.com>
2023-07-04 22:30:13 +0300

    Add sign up screen

---
+++ app/src/main/java/com/oguzdogdu/wallies/presentation/signup/SignupFragment.kt
@@ -0,0 +1,137 @@
+package com.oguzdogdu.wallies.presentation.signup
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.oguzdogdu.wallies.R
+import com.oguzdogdu.wallies.core.BaseBottomSheetDialogFragment
+import com.oguzdogdu.wallies.databinding.FragmentSignupBinding
+import com.oguzdogdu.wallies.util.FieldValidators
+import com.oguzdogdu.wallies.util.showToast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SignupFragment : BaseBottomSheetDialogFragment<FragmentSignupBinding>(FragmentSignupBinding::inflate) {
+    private val viewModel: SignUpViewModel by viewModels()
+    override fun initViews() {
+        super.initViews()
+        binding.editTextEmail.addTextChangedListener(TextFieldValidation(binding.editTextEmail))
+        binding.editTextPassword.addTextChangedListener(TextFieldValidation(binding.editTextPassword))
+    }
+
+    override fun initListeners() {
+        super.initListeners()
+        binding.buttonSignUp.setOnClickListener {
+            viewModel.userSignUp(
+                name = binding.editTextName.text.toString(),
+                surname = binding.editTextSurname.text.toString(),
+                email = binding.editTextEmail.text.toString(),
+                password = binding.editTextPassword.text.toString()
+            )
+        }
+    }
+
+    override fun observeData() {
+        super.observeData()
+        lifecycleScope.launch {
+            viewModel.signUpState.collect {state ->
+                when(state){
+                    is SignUpState.Loading -> {
+
+                    }
+                    is SignUpState.ErrorSignUp -> {
+                        requireView().showToast(
+                            context = requireContext(),
+                            message = state.errorMessage,
+                            duration = Toast.LENGTH_LONG
+                        )
+                    }
+                    is SignUpState.UserSignUp -> {
+                        requireView().showToast(
+                            context = requireContext(),
+                            message = "Kayıt Başarılı",
+                            duration = Toast.LENGTH_LONG
+                        )
+                        this@SignupFragment.dismiss()
+                    }
+
+                    else -> {}
+                }
+            }
+        }
+    }
+    inner class TextFieldValidation(private val view: View) : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            when (view.id) {
+                R.id.editTextEmail -> validateEmail()
+
+                R.id.editTextPassword -> validatePassword()
+            }
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        val email = binding.editTextEmail.text.toString().trim()
+        val emailLayout = binding.emailContainer
+
+        return when {
+            email.isEmpty() -> {
+                emailLayout.error = "Required Field!"
+                emailLayout.requestFocus()
+                false
+            }
+            !FieldValidators.isValidEmail(email) -> {
+                emailLayout.error = "Invalid Email!"
+                emailLayout.requestFocus()
+                false
+            }
+            else -> {
+                emailLayout.isErrorEnabled = false
+                true
+            }
+        }
+    }
+
+    private fun validatePassword(): Boolean {
+        val password = binding.editTextPassword.text.toString().trim()
+        val passwordLayout = binding.passwordContainer
+
+        return when {
+            password.isEmpty() -> {
+                passwordLayout.error = "Required Field!"
+                binding.editTextPassword.requestFocus()
+                false
+            }
+            password.length < 6 -> {
+                passwordLayout.error = "Password can't be less than 6"
+                binding.editTextPassword.requestFocus()
+                false
+            }
+            !FieldValidators.isStringContainNumber(password) -> {
+                passwordLayout.error = "Required at least 1 digit"
+                binding.editTextPassword.requestFocus()
+                false
+            }
+            !FieldValidators.isStringLowerAndUpperCase(password) -> {
+                passwordLayout.error = "Password must contain upper and lower case letters"
+                binding.editTextPassword.requestFocus()
+                false
+            }
+            !FieldValidators.isStringContainSpecialCharacter(password) -> {
+                passwordLayout.error = "One special character required"
+                binding.editTextPassword.requestFocus()
+                false
+            }
+            else -> {
+                passwordLayout.isErrorEnabled = false
+                true
+            }
+        }
+    }
+}
--- domain/src/main/java/com/oguzdogdu/domain/usecase/auth/SignUpUseCase.kt
+++ domain/src/main/java/com/oguzdogdu/domain/usecase/auth/SignUpUseCase.kt
@@ -3,7 +3,7 @@ package com.oguzdogdu.domain.usecase.auth
 import com.oguzdogdu.domain.model.auth.User
 import com.oguzdogdu.domain.repository.Authenticator
 import com.oguzdogdu.domain.wrapper.Resource
-import com.oguzdogdu.domain.wrapper.toResult
+import com.oguzdogdu.domain.wrapper.toResource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -14,6 +14,6 @@ class SignUpUseCase @Inject constructor(
     operator fun invoke(user: User, password: String): Flow<Resource<User>> {
         return flow {
             emit(repository.signUp(user, password))
-        }.toResult()
+        }.toResource()
     }
 }
